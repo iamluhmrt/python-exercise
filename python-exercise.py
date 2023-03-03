7dcce216cd928b853e218873fd2b4b7f4c2b905a
Luiz Henrique Martins <84768178+iamluhmrt@users.noreply.github.com>
2023-03-03 10:06:41 -0300

    Add files via upload

---
+++ python-exercise.py
@@ -0,0 +1,17 @@
+# Exercício 2.1 - Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador python.
+
+# 1°
+calc1 = (10 + (20 * 30))
+print(calc1)
+
+# 2°
+calc2 = (4 ** 2 / 30)
+print('%.3f' %(calc2))
+
+# 3°
+calc3 = (9 ** 2 + 2) * 6 - 1
+print(calc3)
+
+# Exercício 2.2 - Digite a seguinte expressão no interpretador:
+calc4 = (10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2)
+print(calc4)
